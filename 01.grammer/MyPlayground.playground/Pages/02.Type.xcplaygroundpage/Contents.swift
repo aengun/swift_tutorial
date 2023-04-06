e399d00db04987681ed3a5dd117c1a6960ba747b
aengun <aengun@icloud.com>
2023-04-06 22:34:00 +0900

    Lecture 02 Type

---
+++ 01.grammer/MyPlayground.playground/Pages/02.Type.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+//Type
+
+var test = "kim"
+
+test //esc를 누르면 타입을 확인할 수 있음
+
+
+// Type Annotations
+var myName1: String = "lee1"
+var myName2: String = "lee2"
+
+var score1: Int = 40 //40.0 안됨
+var score2: Double = 40
+
+score1 = 100
+//score1 = "hi" //Cannot assign value of type String to int
+
