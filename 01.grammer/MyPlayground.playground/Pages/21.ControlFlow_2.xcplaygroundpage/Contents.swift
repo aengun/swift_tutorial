6b4e03035d6c98b6d27297f9c6ce61b912fffd89
aengun <aengun@icloud.com>
2023-05-08 22:11:41 +0900

    Lecture 21 Control Flow - while

--- 01.grammer/MyPlayground.playground/Pages/21.ControlFlow_2.xcplaygroundpage/Contents.swift
+++ 01.grammer/MyPlayground.playground/Pages/21.ControlFlow_2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// Control Flow(흐름 제어)
 
-//: [Next](@next)
+// while
+// 반복 끝이 정해지지 않을 때
+
+// 주사위(1~6)
+// 주사위 홀수 -1
+// 주사위 짝수 +2
+// 목표 10까지 도달
+
+var dice = 0
+var myPosition = 0
+
+while myPosition < 10 {
+    dice = Int.random(in: 1...6) // 난수 만들기
+    if(dice % 2 == 0) {
+        myPosition += 2
+    } else if myPosition > 0 {
+        myPosition -= 1
+    }
+    
+    print("dice", dice)
+    print("myPosition", myPosition)
+}
+print("end")
